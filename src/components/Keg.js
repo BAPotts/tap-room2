6ab7b4d8eaba269b45f63d4894732ed8b1e2dd5a
Beverly Potts <pottsbeverly@gmail.com>
2020-09-11 15:12:20 -0400

    Add method to keg component

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 function Keg(props){
   return(
     <React.Fragment>
-      <h3>{props.name} - {props.brand}</h3>
-      <h2>{props.price}</h2>
-      <h2>{props.alcoholContent}</h2>
-      <h2>{props.pintsLeft}</h2>
+      <div onClick = {() => props.whenKegClicked(props.id)}>
+        <h3>{props.name} - {props.brand}</h3>
+        <h2>{props.price}</h2>
+        <h2>{props.alcoholContent}</h2>
+        <h2>{props.pintsLeft}</h2>
+      </div>
     </React.Fragment>
   );
 }
@@ -17,6 +19,8 @@ Keg.propTypes = {
   brand: PropTypes.string,
   price: PropTypes.string,
   alcoholContent: PropTypes.string,
-  pintsLeft: PropTypes.number
+  pintsLeft: PropTypes.number,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
 };
 export default Keg;
