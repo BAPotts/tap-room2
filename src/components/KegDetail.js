93767a5462c991306724ce6b38830b8eb41d1ace
Beverly Potts <pottsbeverly@gmail.com>
2020-09-11 15:23:19 -0400

    Update kegdetail to render selectedkeg

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,12 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function KegDetail(props){
+  const { keg } = props;
+
   return (
     <React.Fragment>
       <h1>Keg Details</h1>
+      <h3>{keg.name} - {keg.brand}</h3>
+      <p><em>{keg.price}</em></p>
+      <p><em>{keg.alcoholContent}</em></p>
+      <p><em>{keg.pintsLeft}</em></p>
       <hr/>
     </React.Fragment>
   );
 }
+
+TicketDetail.propTypes = {
+  ticket: PropTypes.object
+};
+
 
 export default KegDetail;
